67c9070e73995934c39bebe3efaccc772dcd15f7
mrinstance <142258555+mrinstance@users.noreply.github.com>
2023-10-13 10:48:32 -0600

    Update and rename CS.Area to Area.cs

---
+++ Area.cs
@@ -0,0 +1,14 @@
+int LadoA;
+int LadoB;
+int area;
+
+Console.WriteLine("Programa para calcular el area de un polígono regular");
+Console.WriteLine("Ingresar el valor de un lado A");
+LadoA= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingresar el valor de un lado B");
+LadoB = Convert.ToInt32(Console.ReadLine());
+
+area = LadoA * LadoB;
+
+Console.WriteLine("El area de polígono es igua
